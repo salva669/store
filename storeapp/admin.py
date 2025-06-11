e2da524dfbc69bd728873037aab2d52c9381dc8a
salva66 <henrymkama@gmail.com>
2025-06-11 20:08:00 +0300

    admin_urls

--- storeapp/admin.py
+++ storeapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from storeapp.models import CustomUser
+
+
+class UserModel(UserAdmin):
+    pass
+
+admin.site.register(CustomUser,UserModel)
